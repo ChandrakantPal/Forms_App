5944955e212687c3b7e840eb255fae9b8a700ad8
sanjay <sanjay161097@gmail.com>
2019-09-24 17:46:19 +0530

    Bottom Nav + routing

--- src/components/Navigation/BottomNav/BottomNav.js
+++ src/components/Navigation/BottomNav/BottomNav.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {BottomNavigation,BottomNavigationAction} from '@material-ui/core';
 import PersonIcon from '@material-ui/icons/Person';
 import ApartmentIcon from '@material-ui/icons/Apartment';
+import { NavLink } from 'react-router-dom';
 
 const bottomNav = (props) => (
     <div>    
-        <BottomNavigation 
-        showLabels>
-        <BottomNavigationAction label="Users" icon={<PersonIcon />} />
-        <BottomNavigationAction label="Sites" icon={<ApartmentIcon />} />
+        <BottomNavigation showLabels>
+            <NavLink to="/" exact>
+                <BottomNavigationAction label="Users" icon={<PersonIcon />} />
+            </NavLink>
+            <NavLink to="/sites">
+                <BottomNavigationAction label="Sites" icon={<ApartmentIcon />} />
+            </NavLink>
         </BottomNavigation>
     </div>
 );
